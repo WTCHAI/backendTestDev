3f5ad6bc8ea0b7e1e413c0b1b395b97a2f520a0b
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-06-15 12:24:25 +0700

    Init Sign in api request

--- pages/api/auth/signin.tsx
+++ pages/api/auth/signin.tsx
@@ -1,6 +1,37 @@
 import { NextApiRequest, NextApiResponse } from "next";
+
 import { prismaDb } from "@/lib/db";
 
-export default function handler(req : NextApiRequest,res : NextApiResponse){
-  
+import bcrypt from "bcrypt"
+
+import { User, UserResponse, userFormType } from "@/interface/user";
+
+export default async function handler(req : NextApiRequest,res : NextApiResponse){
+    if (req.method === "POST"){
+      try{
+        const payload : User = await req.body.json()
+        const encryptedPasswd = await bcrypt.hash(payload.password,Number(process.env.SALT_ROUNDS))
+
+        const response = await prismaDb.user.findUnique({
+          where : {
+            email : payload.email,
+            password : encryptedPasswd
+          }
+        })
+        console.log(response)
+        // const result :  User | null = await response.json()
+        
+
+        return res.json({
+          data : response,
+          message : "Sign up success fully",
+          status : 200
+        })
+      }catch(err){  
+        return res.json({
+            message : "Eror sign up",
+            status : 404
+        })
+      }
+    }
 }
